400df1d168dd9a8f44b195001a209556a40723d4
Julien Lépine <lepi.julien@gmail.com>
2020-10-22 16:34:42 -0400

    Add Font, color and initial view

---
+++ SurveyBuilder/UIReferences/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  SurveyBuilder
+//
+//  Created by redpanda on 2020-10-22.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var lightBeige: UIColor {
+        return UIColor(red: 0.957, green: 0.934, blue: 0.906, alpha: 1)
+    }
+    
+    static var grayBlue: UIColor {
+        return UIColor(red: 0.52, green: 0.492, blue: 0.555, alpha: 1)
+    }
+}
---
+++ SurveyBuilder/UIReferences/fonts/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  SurveyBuilder
+//
+//  Created by redpanda on 2020-10-22.
+//
+
+import UIKit
+
+extension UIFont {
+    static var avertaRegular: UIFont {
+        return UIFont(name: "Averta-Regular", size: UIFont.labelFontSize)!
+    }
+}
+
+
